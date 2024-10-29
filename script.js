1e718180a5e6ee31a83cce875a4d3774199b9a88
maymay2010 <mayaramayrinck@gmail.com>
2024-10-28 22:05:49 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+document.getElementById("contactButton").addEventListener("click", function() {
+    const message = document.getElementById("contactMessage");
+    message.style.display = "block";
+});
